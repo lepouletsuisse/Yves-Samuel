625c2a5a1570786e15f10a7eb90b2ace848844ff
lepouletsuisse <xxpoulletxx@gmail.com>
2015-11-10 14:06:26 +0100

    Maj labo04

--- POO/Labo04/src/Compte/Compte.java
+++ POO/Labo04/src/Compte/Compte.java
@@ -1,9 +1,9 @@
-package Compte;/*
+/*
  -----------------------------------------------------------------------------------
- Laboratoire : 04
- Fichier     : Compte.java
- Auteur(s)   : Samuel Darcey & Yves Athanasiadès
- Date        : 06.11.2015
+ Laboratoire : <nn>
+ Fichier     : hgh.java
+ Auteur(s)   : Samuel Darcey
+ Date        : 08.11.2015
 
  But         : <à compléter>
 
@@ -12,56 +12,243 @@ package Compte;/*
  Compilateur : jdk1.8.0_60
  -----------------------------------------------------------------------------------
 */
+package Compte;
 
+/*
+* TODO: constructeur, verifier bonne implementation, verifier donner,
+* TODO: verifier formatage
+* */
+
+/**
+ *
+ */
 public class Compte {
+
+   private static double defaultRetraitMax = 1000.0;
+   private static double defaultDecouvertMax = 800.0;
+   private final static double DEFAULT_SOLDE = 0.0;
+
+
+   /**
+    *
+    */
    private int id;
+
+   private static int currentId;
+
+   /**
+    *
+    */
    private String titulaire;
+
+   /**
+    *
+    */
    private double solde;
+
+   /**
+    *
+    */
    private double decouvert;
+
+   /**
+    *
+    */
    private double decouvertMax;
+
+   /**
+    *
+    */
    private double retraitMax;
 
-   //Constructeur
-   public Compte(String titulaire){
-      this.titulaire = titulaire;
+   /**
+    * @param nom
+    */
+   public Compte(String nom) throws IllegalArgumentException {
+      this(nom, DEFAULT_SOLDE);
    }
 
-   //get
-   public int getId(){
+   /**
+    * @param nom
+    * @param solde
+    */
+   public Compte(String nom, double solde) throws IllegalArgumentException {
+      titulaire = nom;
+      id = currentId++;
+      setSolde(solde);
+      setDecouvertMax(defaultDecouvertMax);
+      setRetraitMax(defaultRetraitMax);
+
+      if (decouvertMax < 0 || -solde > decouvertMax || retraitMax < 0 || nom == null) {
+         throw new IllegalArgumentException();
+      }
+   }
+
+   /**
+    * @return
+    */
+   public String toString() {
+      return "\nTitulaire: " + titulaire + "\nId: " + id + "\nSolde: " + solde + "\nDecouvert?: "
+              + (estDecouvert() ? "Oui" : "Non") + "\nDecouvert Max: " + decouvertMax + "\nRetrait Max: "
+              + retraitMax + "\n";
+   }
+
+   public static void setDefaultRetraitMax(double montant) {
+      defaultRetraitMax = montant;
+   }
+
+   public static void setDefaultDecouvertMax(double montant) {
+      defaultDecouvertMax = montant;
+   }
+
+   public static double getDefaultRetraitMax() {
+      return defaultRetraitMax;
+   }
+
+   public static double getDefaultDecouvertMax() {
+      return defaultDecouvertMax;
+   }
+
+   public static double getDefaultSolde() {
+      return DEFAULT_SOLDE;
+   }
+
+   /**
+    * @return
+    */
+   public int getId() {
       return id;
    }
 
-   public String getTitulaire(){
+   /**
+    * @return
+    */
+   public String getTitulaire() {
       return titulaire;
    }
 
-   public double getSolde(){
+   /**
+    * @return
+    */
+   public double getSolde() {
       return solde;
    }
 
-   public double getDecouvert(){
+   /**
+    * @return
+    */
+   public double getDecouvert() {
       return decouvert;
    }
 
-   public double getDecouvertMax(){
+   /**
+    * @return
+    */
+   public double getDecouvertMax() {
       return decouvertMax;
    }
 
-   public double getRetraitMax(){
+   /**
+    * @return
+    */
+   public double getRetraitMax() {
       return retraitMax;
    }
 
-   //Set
+   /**
+    * @param montant
+    * @return
+    */
+   private void setSolde(double montant) {
+      if (montant >= 0) {
+         solde = montant;
+         actualiserDecouvert();
+      }
+   }
 
-   public void crediter(double montant){
+   /**
+    * @param montant
+    * @return
+    */
+   public void setDecouvertMax(double montant) {
+      if (montant >= 0) {
+         decouvertMax = montant;
+      }
+   }
+
+   /**
+    * @param montant
+    * @return
+    */
+   public void setRetraitMax(double montant) {
+      if (montant >= 0) {
+         retraitMax = montant;
+      }
+   }
+
+   /**
+    * @return
+    */
+   public boolean estDecouvert() {
+      return decouvert > 0;
+   }
+
+   public double debitMaxAutorise() {
+      if (Math.abs(solde - retraitMax) > decouvertMax) {
+         return retraitMax;
+      }
+      return solde + decouvertMax;
+   }
+
+   /**
+    * @param montant
+    * @return
+    */
+   public boolean crediter(double montant) {
+      if (montant < 0) {
+         return false;
+      }
       solde += montant;
+      actualiserDecouvert();
+      return true;
    }
 
-   public void debiter(double montant){
+   /**
+    * @param montant
+    * @return
+    */
+   public double debiter(double montant) {
+      if (montant < 0) {
+         return 0.0d;
+      }
+      if (montant > retraitMax) {
+         montant = retraitMax;
+      }
+      if (Math.abs(solde - montant) > decouvertMax) {
+         montant = solde + decouvertMax;
+      }
       solde -= montant;
+      actualiserDecouvert();
+      return montant;
    }
 
-   public void virer(Compte credit, Compte debit){
-
+   /**
+    * @param debit
+    * @param credit
+    * @return
+    */
+   public static double virer(Compte debit, Compte credit, double montant) {
+      montant = debit.debiter(montant);
+      credit.crediter(montant);
+      return montant;
    }
+
+   private void actualiserDecouvert() {
+      if (solde < 0) {
+         decouvert = Math.abs(solde);
+      } else {
+         decouvert = 0;
+      }
+   }
+
 }
--- POO/Labo04/src/Compte/Main.java
+++ POO/Labo04/src/Compte/Main.java
@@ -3,7 +3,7 @@
  Laboratoire : <nn>
  Fichier     : Main.java
  Auteur(s)   : Samuel Darcey
- Date        : 06.11.2015
+ Date        : 08.11.2015
 
  But         : <à compléter>
 
@@ -16,7 +16,34 @@
 package Compte;
 
 public class Main {
-   public void main(String args[]){
+   public static void main(String args[]){
+
+      Compte c1 = new Compte("J. DUPONT", 1000);
+      c1.setDecouvertMax(1000);
+
+      Compte c2 = new Compte("C. DURAND");
+
+      System.out.print(c1);
+      System.out.print(c2);
+
+      c1.debiter(500);
+      System.out.print(c1);
+
+      c2.crediter(500);
+      System.out.print(c1);
+
+      for(int i = 0 ; i < 3 ; i++){
+         c1.debiter(5000);
+         System.out.print(c1);
+      }
+
+      c1.crediter(500);
+      System.out.print(c1);
+
+      Compte.virer(c1, c2, 500);
+
+      System.out.print(c1);
+      System.out.print(c2);
 
    }
 }
